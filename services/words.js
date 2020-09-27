3fd5c5bf9428c0512636bac2241069630af4a80a
ijemmao <ijemmaonwuzulike10@gmail.com>
2020-09-27 11:01:35 -0700

    Ignore prefixed dash when searching for verbs

--- services/words.js
+++ services/words.js
@@ -1,17 +1,22 @@
 import { keys } from 'lodash';
 import dictionary from '../ig/dictionaries/ig-en_expanded.json';
 
-const findSearchWord = (regexWord, word) => {
+/* Removes the verb prefix character '-' */
+const removePrefix = (term) => {
+    if (term.charAt(0) === '-') {
+        return term.substring(1);
+    }
+    return term;
+}
+
+export const findSearchWord = (regexWord, word) => {
     const results = keys(dictionary).reduce((matchedResults, key) => {
         const termInformation = dictionary[key];
-        if (key.match(regexWord) && key.length === word.length) {
+        const trimmedKey = removePrefix(key);
+        if (trimmedKey.match(regexWord) && trimmedKey.length === word.length) {
             matchedResults[key] = termInformation;
         }
         return matchedResults;
     }, {});
     return results;
-}
-
-export {
-    findSearchWord,
 }
