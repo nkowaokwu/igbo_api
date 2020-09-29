15fc99c5aa39d9cb1eea23dfdc07d2f3ea7cf91b
ijemmao <ijemmaonwuzulike10@gmail.com>
2020-09-29 07:42:09 -0700

    Use constants for current column

--- ig/parseAndBuild.js
+++ ig/parseAndBuild.js
@@ -2,7 +2,7 @@ import { last } from 'lodash';
 import { getLeftAndTopStyles, getChildrenText } from './utils/parseHelpers';
 import writeToFiles from './utils/writeToFiles';
 import { clean, normalize } from '../utils/normalization';
-import { COLUMN_MAP, SAME_CELL_TOP_DIFFERENCE } from '../shared/constants/parseConstants';
+import { COLUMNS, LEFT_STYLE_TO_COLUMN, SAME_CELL_TOP_DIFFERENCE } from '../shared/constants/parseConstants';
 
 const normalizationMap = {};
 
@@ -27,12 +27,12 @@ const insertTermInNormalizationMap = (normalizedTerm, naturalTerm) => {
 }
 
 const buildDictionary = (span, dictionary, options = {}) => {
-    const currentColumn = COLUMN_MAP[getLeftAndTopStyles(span).left];
+    const currentColumn = LEFT_STYLE_TO_COLUMN[getLeftAndTopStyles(span).left];
     const naturalChildrenText = getChildrenText(span);
     const childrenText = options.normalize ? normalize(naturalChildrenText) : naturalChildrenText;
 
     switch (currentColumn) {
-        case 'left':
+        case COLUMNS.LEFT:
             const cleanedChildrenText = clean(childrenText);
             const cleanedNaturalChildrenText = clean(naturalChildrenText);
             options.normalize && insertTermInNormalizationMap(cleanedChildrenText, cleanedNaturalChildrenText);
@@ -47,17 +47,14 @@ const buildDictionary = (span, dictionary, options = {}) => {
             centerCount = 0; // Reset the center count for a new word
             currentPhrase = '';
             break;
-        case 'center':
+        case COLUMNS.CENTER:
             // centerCount keeps track of how many times you are in the center column
             // Great for identifying word classes vs phrases
             centerCount += 1;
-            if (prevColumn === 'left') {
+            if (prevColumn === COLUMNS.LEFT) {
                 // Word class
                 last(dictionary[currentWord]).wordClass = childrenText;
-            } else if (prevColumn === 'right' || prevColumn === 'center') {
-                if (currentWord === '-zu-zò') {
-                    console.log('ok')
-                }
+            } else if (prevColumn === COLUMNS.RIGHT || prevColumn === COLUMNS.CENTER) {
                 currentPhrase = childrenText;
                 if (!!currentPhrase) {
                     last(dictionary[currentWord]).phrases = {
@@ -67,21 +64,21 @@ const buildDictionary = (span, dictionary, options = {}) => {
                 }
             }
             break;
-        case 'right':
+        case COLUMNS.RIGHT:
             const isSameCell = getLeftAndTopStyles(span).top - getLeftAndTopStyles(prevSpan).top === SAME_CELL_TOP_DIFFERENCE;
-            if (prevColumn === 'center' && centerCount < 2) {
+            if (prevColumn === COLUMNS.CENTER && centerCount < 2) {
                 last(dictionary[currentWord]).definition = childrenText;
-            } else if (isSameCell && prevColumn === 'right' && centerCount < 2) {
+            } else if (isSameCell && prevColumn === COLUMNS.RIGHT && centerCount < 2) {
                 const currentDefinition = last(dictionary[currentWord]).definition;
                 last(dictionary[currentWord]).definition = currentDefinition + childrenText;
-            } else if (prevColumn === 'right' && centerCount < 2) {
+            } else if (prevColumn === COLUMNS.RIGHT && centerCount < 2) {
                 last(dictionary[currentWord]).examples.push(childrenText)
-            } else if (prevColumn === 'center' && centerCount >= 2) {
+            } else if (prevColumn === COLUMNS.CENTER && centerCount >= 2) {
                 // Grab the current phrase and then add to definition
                 if (!!currentPhrase) {
                     last(dictionary[currentWord]).phrases[currentPhrase].definition = childrenText;
                 }
-            } else if (prevColumn === 'right' && centerCount >= 2) {
+            } else if (prevColumn === COLUMNS.RIGHT && centerCount >= 2) {
                 // Append the current example to the currentPhrase
                 if (!!currentPhrase) {
                     last(dictionary[currentWord]).phrases[currentPhrase].examples.push(childrenText);
