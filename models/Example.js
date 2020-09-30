88185ae9948608ba9709c4695c6b23a749db3862
ijemmao <ijemmaonwuzulike10@gmail.com>
2020-09-30 16:13:57 -0700

    Create MongoDB Schemas

---
+++ models/Example.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const { Schema, Types } = mongoose;
+const exampleSchema = new Schema({
+    example: String,
+    parentWord: { type: Types.ObjectId, ref: 'Word' },
+    parentPhrase: { type: Types.ObjectId, ref: 'Phrase' },
+});
+
+export default mongoose.model('Example', exampleSchema);
---
+++ models/Phrase.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const { Schema, Types } = mongoose;
+const phraseSchema = new Schema({
+    phrase: String,
+    parentWord: { type: Types.ObjectId, ref: 'Word' },
+    definitions: [{ type: String }],
+    examples: [{ type: Types.ObjectId, ref: 'Example' }]
+});
+
+export default mongoose.model('Phrase', phraseSchema);
