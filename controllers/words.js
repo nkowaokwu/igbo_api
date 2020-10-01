20ddc1a1f8c2782769a885d5909c25620750fc88
ijemmao <ijemmaonwuzulike10@gmail.com>
2020-09-30 22:15:00 -0700

    Create populate route to seed mongo database

--- controllers/words.js
+++ controllers/words.js
@@ -1,7 +1,12 @@
+import { keys, reduce, map } from 'lodash';
 import removePrefix from '../shared/utils/removePrefix';
+import Word from '../models/Word';
 import { findSearchWord } from '../services/words';
 import { NO_PROVIDED_TERM } from '../utils/constants/errorMessages';
 import diacriticCodes from '../shared/constants/diacriticCodes';
+import { getDocumentsIds } from '../shared/utils/documentUtils';
+import { createPhrase } from './phrases';
+import { createExample } from './examples';
 
 export const createRegExp = (searchWord) => {
     const regexWordString = [...searchWord].reduce((regexWord, letter) => {
@@ -19,6 +24,45 @@ const getWordData = (_, res) => {
     }
     const regexWord = createRegExp(searchWord);
     return res.send(findSearchWord(regexWord, searchWord));
+};
+
+export const createWord = async (data) => {
+    const { examples, word, wordClass, definitions } = data;
+    const wordData = {
+        word,
+        wordClass,
+        definitions,
+    };
+    const newWord = new Word(wordData);
+    await newWord.save();
+    
+    /* Go through each word's phrase and create a Phrase document */
+    const phrases = keys(data.phrases);
+    const savedPhrases = reduce(phrases, (phrasePromises, phrase) => {
+        const phraseInfo = data.phrases[phrase];
+        phraseInfo.phrase = phrase;
+        phraseInfo.word = newWord.id;
+        phrasePromises.push(createPhrase(phraseInfo));
+        return phrasePromises;
+    }, []);
+
+    /* Go through each word's example and create an Example document */
+    const savedExamples = map(examples, async (example) => {
+        const exampleData = {
+            example,
+            parentWord: newWord.id,
+        };
+        return createExample(exampleData);
+    });
+    
+    /* Wait for all the Phrases and Examples to be created and then add them to the Word document */
+    return Promise.all([savedPhrases, savedExamples]).then((res) => {
+        const phraseIds = getDocumentsIds(res[0]);
+        const exampleIds = getDocumentsIds(res[1]);
+        newWord.phrases = phraseIds;
+        newWord.examples = exampleIds;
+        return newWord.save();
+    })
 };
 
 export { getWordData };
--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 import express from 'express';
-import mongodb from 'mongodb';
+import mongoose from 'mongoose';
 import { getWordData } from './controllers/words';
+import { createWord } from './controllers/words';
+import dictionary from './dictionaries/ig-en/ig-en.json';
 
-const { MongoClient } = mongodb;
 const app = express();
 const router = express.Router();
 const port = 8080;
-const url = 'mongodb://localhost:27017';
 const dbName = 'igbo_api';
+const url = `mongodb://localhost:27017/${dbName}`;
 
-MongoClient.connect(url, (_, client) => {
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', function() {
     console.log('🗄 Database is connected');
-    client.db(dbName);
 });
 
 app.get('/', (_, res) => {
     res.send('Hello World!');
+});
+
+/* This route will populate a local MongoDB database */
+app.post('/populate', (_, res) => {
+    if (process.env.NODE_ENV === 'dev') {
+        mongoose.connection.db.dropDatabase();
+        for(const key in dictionary) {
+            const value = dictionary[key];
+            value.forEach((term) => {
+                term.word = key
+                createWord(term);
+            });
+        }
+    }
+    res.redirect('/');
 });
 
 router.get('/', (_, res) => {
@@ -24,7 +42,7 @@ router.get('/', (_, res) => {
 
 router.get('/words', getWordData);
 
-app.use('*', (req, res, next) => {
+app.use('*', (req, _, next) => {
     if (process.env.NODE_ENV === 'dev') {
         console.log(req.query);
     }
