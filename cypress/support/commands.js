86a99ca214a9f8343c2e73ff77c25d22dd514d08
ijemmao <ijemmaonwuzulike10@gmail.com>
2020-10-09 23:01:45 -0700

    Add Cypress to the project

---
+++ cypress/support/commands.js
@@ -0,0 +1,32 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('searchDictionary', (keyword) => {
+    cy.server();
+    cy.route(`https://cors-anywhere.herokuapp.com/http://igbo-api.herokuapp.com/api/v1/search/words?keyword=${keyword}`).as('getWord');
+    cy.get('[data-test="search-bar"]').type(keyword);
+    cy.contains('Search').click();
+});
