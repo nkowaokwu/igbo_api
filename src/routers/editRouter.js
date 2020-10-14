3d06f98a552ec65527add02e5e2c75edfd479141
ijemmao <ijemmaonwuzulike10@gmail.com>
2020-10-14 15:27:39 -0700

    Add swagger comments

--- src/routers/editRouter.js
+++ src/routers/editRouter.js
@@ -6,9 +6,147 @@ const editRouter = express.Router();
 
 /* These routes are used to allow users to suggest new words and examples */
 if (process.env.NODE_ENV !== 'production') { // TODO: remove this guard rail when releasing for production
+  /**
+ * @swagger
+ * /edit/words:
+ *   post:
+ *     description: Creates a new WordSuggestion document
+ *     tags:
+ *      - development
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         description: The word suggestion that will be created in the database
+ *         schema:
+ *           type: object
+ *           properties:
+ *             word:
+ *               type: string
+ *             wordClass:
+ *               type: string
+ *             definitions:
+ *               type: array
+ *               items:
+ *                 type: string
+ *             variations:
+ *               type: array
+ *               items:
+ *                 type: string
+ *     responses:
+ *      200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                id:
+ *                type: string
+ *   put:
+ *      description: Updates an existing WordSuggestion document
+ *      tags:
+ *       - development
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *       - in: body
+ *         name: body
+ *         description: The updated word suggested data
+ *         schema:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: string
+ *             word:
+ *               type: string
+ *             wordClass:
+ *               type: string
+ *             definitions:
+ *               type: array
+ *               items:
+ *                 type: string
+ *             variations:
+ *               type: array
+ *               items:
+ *                 type: string
+ *      responses:
+ *        200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
   editRouter.post('/words', postWordSuggestion);
   editRouter.put('/words', putWordSuggestion);
 
+  /**
+ * @swagger
+ * /edit/example:
+ *   post:
+ *     description: Creates a new ExampleSuggestion document
+ *     tags:
+ *      - development
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         description: The example suggestion that will be created in the database
+ *         schema:
+ *           type: object
+ *           properties:
+ *             igbo:
+ *               type: string
+ *             english:
+ *               type: string
+ *             associatedWords:
+ *               type: array
+ *               items:
+ *                 type: string
+ *     responses:
+ *      200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                id:
+ *                type: string
+ *   put:
+ *      description: Updates and existing ExampleSuggestion document
+ *      tags:
+ *       - development
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *       - in: body
+ *         name: body
+ *         description: The updated example suggested data
+ *         schema:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: string
+ *             igbo:
+ *               type: string
+ *             english:
+ *               type: string
+ *             associatedWords:
+ *               type: array
+ *               items:
+ *                 type: string
+ *      responses:
+ *        200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
   editRouter.post('/examples', postExampleSuggestion);
   editRouter.put('/examples', putExampleSuggestion);
 }
