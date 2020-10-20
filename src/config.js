a2c98d766c3b4a29fb0bd981fab94d5ee5450307
Ebube Chuba <ebube97@me.com>
2020-10-19 21:18:19 -0700

    update API call to new route

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 const API_ROOT = process.env.NODE_ENV === 'production' ? 'http://igboapi.com' : 'https://cors-anywhere.herokuapp.com/http://www.igboapi.com';
 
-export default `${API_ROOT}/api/v1/search/words`;
+export default `${API_ROOT}/api/v1/words`;
