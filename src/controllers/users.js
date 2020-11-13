c8e230722e8c218f802f817c7a48465d60e91801
Ebube Chuba <ebube97@me.com>
2020-11-12 21:43:16 -0800

    add ability to list users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,14 @@
 /* Get all users from Firebase */
-export const getUsers = (req, res) => {
-  // TODO: Use Firebase functions to get all user objects
+import * as admin from 'firebase-admin';
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+export const getUsers = async (req, res) => {
+  // Use Firebase functions to get all user objects
   // This will allow for selecting individual users and updating their user roles
-  res.send('send back all users');
+  const result = await admin.auth().listUsers();
+  const users = result.users.map((userRecord) => userRecord.toJSON());
+  res.send(users);
 };
