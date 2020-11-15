6f5ad3b7587b63c21cca0c6b3c11088beaff0e0a
ijemmao <ijemmaonwuzulike10@gmail.com>
2020-11-14 20:16:06 -0800

    Replace logo with an svg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../assets/images/logo.svg';
 
 const Navbar = () => (
   <ul className="responsive-container justify-between items-center flex px-6 lg:px-3 py-5 text-green-900">
-    <h1 id="logo" className="font-bold text-2xl text-gray-800">
-      <a href="/">Nkowa okwu</a>
-    </h1>
+    <a href="/">
+      <img id="logo" src={Logo} alt="Logo that says 'Nkowa okwu'" />
+    </a>
     <div className="flex space-x-6 lg:space-x-10">
       <li><a href="/" className="transition-all duration-300 hover:text-green-500">Search</a></li>
       <li><a href="/about" className="transition-all duration-300 hover:text-green-500">About</a></li>
