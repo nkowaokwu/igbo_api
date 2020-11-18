d9b951a88490e540e8a2801ef11d0596419d875c
Ebube Chuba <ebube97@me.com>
2020-11-17 16:48:29 -0800

    set status

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,5 +6,6 @@ export const getUsers = async (req, res) => {
   // This will allow for selecting individual users and updating their user roles
   const result = await admin.auth().listUsers();
   const users = result.users.map((userRecord) => userRecord.toJSON());
+  res.status(200);
   res.send(users);
 };
