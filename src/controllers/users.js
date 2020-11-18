128d6e212085c2940099591341afcd68b42ddbe9
Ebube Chuba <ebube97@me.com>
2020-11-17 16:48:58 -0800

    admin.default again

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,7 +4,7 @@ import * as admin from 'firebase-admin';
 export const getUsers = async (req, res) => {
   // Use Firebase functions to get all user objects
   // This will allow for selecting individual users and updating their user roles
-  const result = await admin.auth().listUsers();
+  const result = await admin.default.auth().listUsers();
   const users = result.users.map((userRecord) => userRecord.toJSON());
   res.status(200);
   res.send(users);
