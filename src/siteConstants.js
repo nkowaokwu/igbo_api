11784c3e140f23fe4ce79fe4deff1f717600735b
ijemmao <ijemmaonwuzulike10@gmail.com>
2021-01-03 17:32:53 -0800

    Server Side Render Homepage

--- src/siteConstants.js
+++ src/siteConstants.js
@@ -1,6 +1,6 @@
 /* This file includes the constants necessary to power the API homepage */
 export const PORT = process.env.PORT || 8080;
-export const API_ROUTE = process.env.NODE_ENV === 'production' ? '' : `http://localhost:${PORT}`;
+export const API_ROUTE = process.env.NODE_ENV === 'production' ? 'https://igboapi.com' : `http://localhost:${PORT}`;
 export const DICTIONARY_APP_URL = process.env.NODE_ENV === 'production'
   ? 'https://nkowaokwu.com'
   : 'http://localhost:8000'; // A local instance of the dictionary app must be running
