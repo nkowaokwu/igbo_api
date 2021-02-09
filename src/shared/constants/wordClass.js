52ea1c1d731428555d28ad8a2e088342d1081ca3
Gospel Chinyereugo (Codebug) <gospelokpara@gmail.com>
2021-02-09 05:40:54 +0100

    feat: Create Igbo Part of Speech Tagset Constant

---
+++ src/shared/constants/wordClass.js
@@ -0,0 +1,62 @@
+export default {
+  ADJ: {
+    label: 'Adjective',
+    description: 'Adjective',
+  },
+  ADV: {
+    label: 'Adverb',
+    description: 'Adverb',
+  },
+  CD: {
+    label: 'Number',
+    description: 'Number',
+  },
+  CJN: {
+    label: 'Conjunction',
+    description: 'Conjunction',
+  },
+  DEM: {
+    label: 'Demonstration',
+    description: 'Demonstration',
+  },
+  FW: {
+    label: 'Foreign word',
+    description: 'Foreign word',
+  },
+  INTJ: {
+    label: 'Interjection',
+    description: 'Interjection',
+  },
+  NNC: {
+    label: 'Common noun',
+    description: 'Common noun',
+  },
+  NNP: {
+    label: 'Proper noun',
+    description: 'Proper noun',
+  },
+  PREP: {
+    label: 'Preposition',
+    description: 'Preposition',
+  },
+  PRN: {
+    label: 'Pronoun',
+    description: 'Pronoun',
+  },
+  QTF: {
+    label: 'Quantifer',
+    description: 'Quantifer',
+  },
+  SYM: {
+    label: 'Punctuations',
+    description: 'Punctuations',
+  },
+  V: {
+    label: 'Verb',
+    description: 'Verb',
+  },
+  WH: {
+    label: 'Interrogative',
+    description: 'Interrogative',
+  },
+};
