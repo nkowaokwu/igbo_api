0f82c23444c2d08192d8de8b6f3b9b9b62509495
ijemmao <ijemmaonwuzulike10@gmail.com>
2021-03-19 18:22:30 -0700

    docs: create Flags and expose dialects option

--- src/pages/components/Demo/Demo.js
+++ src/pages/components/Demo/Demo.js
@@ -1,25 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { omit } from 'lodash';
+import queryString from 'query-string';
 import JSONPretty from 'react-json-pretty';
+import { Input, Checkbox } from 'antd';
 import { API_ROUTE, DICTIONARY_APP_URL } from '../../../siteConstants';
 
 const Demo = ({ searchWord, words }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [keyword, setKeyword] = useState(searchWord);
+  const [queries, setQueries] = useState({});
+  const [initialQueries, setInitialQueries] = useState({});
   const [productionUrl, setProductionUrl] = useState('');
   const responseBody = JSON.stringify(words, null, 4);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setProductionUrl(window.origin);
+      setInitialQueries(queryString.parse(window.location.search));
+      setIsLoading(false);
       if (keyword) {
         window.location.hash = 'try-it-out';
       }
     }
   }, []);
 
-  const onSubmit = (e) => {
-    e?.preventDefault();
-    window.location.href = `/?word=${keyword}`;
+  const constructQueryString = () => {
+    const queriesString = queryString.stringify(queries);
+    return queriesString ? `&${queriesString}` : '';
+  };
+
+  const onSubmit = (e = {}) => {
+    e.preventDefault();
+    const appendQueries = constructQueryString();
+    window.location.href = `/?word=${keyword}${appendQueries}`;
   };
 
   const onEnter = (e) => {
@@ -28,7 +42,20 @@ const Demo = ({ searchWord, words }) => {
     }
   };
 
-  return (
+  const constructRequestUrl = () => {
+    const appendQueries = constructQueryString();
+    return `${productionUrl || API_ROUTE}/api/v1/words?keyword=${keyword}${appendQueries}`;
+  };
+
+  const handleDialects = ({ target }) => {
+    if (target.checked) {
+      setQueries({ ...queries, dialects: target.checked });
+    } else {
+      setQueries(omit(queries, ['dialects']));
+    }
+  };
+
+  return !isLoading ? (
     <div className="flex justify-center mb-16">
       <div className="flex flex-col items-center md:items-start lg:flex-row lg:space-x-10">
         <div className="demo-inputs-container space-y-5">
@@ -36,18 +63,29 @@ const Demo = ({ searchWord, words }) => {
             <p className="self-center md:self-start">
               {'Enter a word in either English or Igbo to see it\'s information'}
             </p>
-            <input
+            <Input
+              size="large"
               onInput={(e) => setKeyword(e.target.value)}
               onKeyPress={onEnter}
-              className="h-12 w-full border-current border-solid border-2 rounded-md px-3 py-5"
+              className="h-12 w-full border-gray-600 border-solid border-2 rounded-md px-3 py-5"
               placeholder="i.e. please or biko"
               defaultValue={searchWord}
             />
-            <input
+            <Input
               disabled
-              value={`${productionUrl || API_ROUTE}/api/v1/words?keyword=${keyword}`}
+              value={constructRequestUrl()}
               className="w-full py-3 px-5"
             />
+            <h2 className="text-2xl">Flags</h2>
+            <div className="px-3 ">
+              <Checkbox
+                className="flex items-center space-x-2"
+                defaultChecked={initialQueries.dialects}
+                onChange={handleDialects}
+              >
+                Dialects
+              </Checkbox>
+            </div>
             <button
               type="submit"
               className="primary-button w-full"
@@ -72,7 +110,7 @@ const Demo = ({ searchWord, words }) => {
         </div>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 Demo.propTypes = {
