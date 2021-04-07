9ed2dd7c111fb917b8355d36af4ff7ba79d44f66
gauravsingh <gauravsinghsisodia5@gmail.com>
2021-04-07 23:15:18 +0530

    fix: url on server side

--- src/siteConstants.js
+++ src/siteConstants.js
@@ -1,5 +1,7 @@
 /* This file includes the constants necessary to power the API homepage */
 export const PORT = process.env.PORT || 8080;
 export const API_ROUTE = process.env.HEROKU ? 'https://igboapi.com' : `http://localhost:${PORT}`;
-export const DICTIONARY_APP_URL = 'https://nkowaokwu.com';
+export const DICTIONARY_APP_URL = process.env.HEROKU
+  ? 'https://nkowaokwu.com'
+  : 'http://localhost:8000';
 export const API_FROM_EMAIL = process.env.API_FROM_EMAIL || 'igboapi@gmail.com';
