ce68cc6196475e2bc171115ebe874759637dd3ca
gauravsingh <gauravsinghsisodia5@gmail.com>
2021-04-13 23:02:21 +0530

    chore: add comment

--- src/siteConstants.js
+++ src/siteConstants.js
@@ -3,5 +3,5 @@ export const PORT = process.env.PORT || 8080;
 export const API_ROUTE = process.env.HEROKU ? 'https://igboapi.com' : `http://localhost:${PORT}`;
 export const DICTIONARY_APP_URL = process.env.HEROKU
   ? 'https://nkowaokwu.com'
-  : 'http://localhost:8000';
+  : 'http://localhost:8000'; // A local instance of the dictionary app must be running
 export const API_FROM_EMAIL = process.env.API_FROM_EMAIL || 'igboapi@gmail.com';
