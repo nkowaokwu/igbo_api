b843b2bd611a53726f1a3dba81b4f400ef2712ef
Chriswenzy <chrischukwuebuka85@gmail.com>
2021-09-21 16:49:20 +0100

    design for cards

--- src/pages/components/Card/Card.js
+++ src/pages/components/Card/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Card = ({ title, description }) => (
+const Card = ({ title, description, icon }) => (
   <div
     className={`card-container  card-custom-border flex flex-col justify-center 
     shadow-xl rounded-lg py-3 px-5 my-10 select-none`}
   >
-    <i className="rounded-full h-16 w-16 bg-white my-4 justify-center text-center">icon</i>
+    <span className="rounded-full h-16 w-16 bg-white my-4 justify-center text-center">
+      <i className={icon}> </i>
+    </span>
     <h1 className="text-2xl font-bold text-gray-900">{title}</h1>
     <p className="text-l text-gray-700">{description}</p>
   </div>
@@ -15,11 +17,13 @@ const Card = ({ title, description }) => (
 Card.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
+  icon: PropTypes.string,
 };
 
 Card.defaultProps = {
   title: '',
   description: '',
+  icon: '',
 };
 
 export default Card;
