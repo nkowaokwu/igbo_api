fcbe86a8186cdd4a957f924bdf34988e6ceb1684
Chriswenzy <chrischukwuebuka85@gmail.com>
2021-09-21 16:50:00 +0100

    add link for fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -20,6 +20,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
         </Head>
         <body>
           <Main />
--- src/pages/components/Statistics/Statistics.js
+++ src/pages/components/Statistics/Statistics.js
@@ -1,29 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 // import PropTypes from 'prop-types';
 
-const Statistics = () => (
-  <div className="flex bg-gray-200">
-    <div className="flex-auto text-gray-700 text-center custom-border bg-gray-400 px-4 py-4 m-8">
-      <h1 className="text-6xl">8,000+</h1>
-      Words in the database
-    </div>
-    <div className="flex-auto text-gray-700 text-center custom-border bg-gray-400 px-4 py-2 m-8">
-      <h1 className="text-6xl">2,000+</h1>
-      Example sentences
-    </div>
+const Statistics = () => {
+  const [contributorDetails, setContributorsDetails] = useState(null);
 
-    <div className="flex text-gray-700 text-center custom-border bg-gray-400 px-4 py-2 m-8">
-      <div className="flex-auto text-gray-700 px-4 py-2 m-2">
-        <h1 className="text-6xl">750+</h1>
-        Audio pronunciations for words
+  useEffect(() => {
+    axios.get('https://api.github.com/repos/ijemmao/igbo_api/contributors')
+      .then((response) => {
+        // console.log(response.data);
+        setContributorsDetails(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div>
+      <div className="flex bg-gray-200">
+        <div className="flex-auto text-gray-700 text-center custom-border bg-gray-400 px-4 py-4 m-8">
+          <h1 className="text-6xl">8,000+</h1>
+          Words in the database
+        </div>
+        <div className="flex-auto text-gray-700 text-center custom-border bg-gray-400 px-4 py-2 m-8">
+          <h1 className="text-6xl">2,000+</h1>
+          Example sentences
+        </div>
+
+        <div className="flex text-gray-700 text-center custom-border bg-gray-400 px-4 py-2 m-8">
+          <div className="flex-auto text-gray-700 px-4 py-2 m-2">
+            <h1 className="text-6xl">750+</h1>
+            Audio pronunciations for words
+          </div>
+          <div className="flex-auto text-gray-700 px-4 py-2 m-2">
+            <h1 className="text-6xl">250+</h1>
+            Words marked as Central Igbo
+          </div>
+        </div>
       </div>
-      <div className="flex-auto text-gray-700 px-4 py-2 m-2">
-        <h1 className="text-6xl">250+</h1>
-        Words marked as Central Igbo
+      <div className="flex bg-gray-200">
+        <div className="flex text-gray-700 text-center custom-border bg-gray-400 px-4 py-2 m-8">
+          <div className="flex-auto text-gray-700 px-4 py-2 m-2">
+            {contributorDetails && (
+            <h1 className="text-6xl">
+              {contributorDetails.length}
+              +
+            </h1>
+            )}
+            Github Contributors
+          </div>
+          <div className="grid grid-rows-2 grid-flow-col gap-0">
+            {contributorDetails && contributorDetails.slice(0, 10).map((contr) => (
+              <div>
+                <img
+                  src={contr.avatar_url}
+                  className=" text-gray-700 bg-gray-400  w-12  m-2 rounded-full"
+                  alt="github avatar"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="text-gray-700 text-center custom-border w-64 bg-gray-400 px-4 py-4 m-8">
+          <h1 className="text-6xl">80+</h1>
+          Members in the Slack
+        </div>
+        <div className=" text-gray-700 text-center custom-border w-64 bg-gray-400 px-4 py-2 m-8">
+          <h1 className="text-6xl">16+</h1>
+          GitHub stars
+        </div>
+
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Statistics.defaultProps = {
   title: '',
