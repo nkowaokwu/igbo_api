0a60be8a6dc552da74ff63436ccc37a188ab02b9
Chriswenzy <chrischukwuebuka85@gmail.com>
2021-11-11 11:12:26 +0100

    remove comment and design fix

--- src/pages/components/Card/Card.js
+++ src/pages/components/Card/Card.js
@@ -7,7 +7,7 @@ const Card = ({ title, description, icon }) => (
     shadow-xl rounded-lg py-3 px-5 my-10 select-none`}
   >
     <span className="rounded-full h-16 w-16 bg-white my-4 justify-center text-center">
-      <i className={icon}> </i>
+      <i className={icon} />
     </span>
     <h1 className="text-2xl font-bold text-gray-900">{title}</h1>
     <p className="text-l text-gray-700">{description}</p>
