fd7fedf06eba048a2bd84b388590113e18d4a036
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2022-01-29 08:51:51 -0500

    chore(analytics): removed malformed google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -21,15 +21,6 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/assets/favicon/site.webmanifest" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-          <!-- Google Analytics -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-WPWJ7T9V9Z"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-WPWJ7T9V9Z');
-          </script>
         </Head>
         <body>
           <Main />
