f35403859d8ce58885181493fb8e9cf424fab56b
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-04-05 08:15:09 -0400

    refactor: normalize strings while sorting results

--- src/controllers/utils/index.js
+++ src/controllers/utils/index.js
@@ -24,8 +24,14 @@ export const sortDocsBy = (searchWord, docs, key) => (
   docs.sort((prevDoc, nextDoc) => {
     const prevDocValue = get(prevDoc, key);
     const nextDocValue = get(nextDoc, key);
-    const prevDocDifference = stringSimilarity.compareTwoStrings(searchWord, diacriticless(prevDocValue)) * 100;
-    const nextDocDifference = stringSimilarity.compareTwoStrings(searchWord, diacriticless(nextDocValue)) * 100;
+    const prevDocDifference = stringSimilarity.compareTwoStrings(
+      searchWord.normalize('NFD'),
+      diacriticless(prevDocValue).normalize('NFD'),
+    ) * 100;
+    const nextDocDifference = stringSimilarity.compareTwoStrings(
+      searchWord.normalize('NFD'),
+      diacriticless(nextDocValue).normalize('NFD'),
+    ) * 100;
     if (prevDocDifference === nextDocDifference) {
       return 0;
     }
