1329e16ee40600e068baa32c5fd9957b2947e222
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-04-06 09:12:00 -0400

    feat(tenses): add tenses field

---
+++ src/shared/constants/Tenses.js
@@ -0,0 +1,26 @@
+export default {
+  INFINITIVE: {
+    value: 'infinitive',
+    label: 'Infinitive',
+  },
+  IMPERATIVE: {
+    value: 'imperative',
+    label: 'Imperative',
+  },
+  SIMPLE_PAST: {
+    value: 'simplePast',
+    label: 'Simple Past',
+  },
+  SIMPLE_PRESENT: {
+    value: 'simplePresent',
+    label: 'Simple Present',
+  },
+  PRESENT_CONTINUOUS: {
+    value: 'presentContinuous',
+    label: 'Present Continuous',
+  },
+  FUTURE: {
+    value: 'future',
+    label: 'Future',
+  },
+};
