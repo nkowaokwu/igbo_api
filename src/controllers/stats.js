4361467e840281e920ee815699294f08120cc093
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-07-25 14:47:26 -0400

    feat(homepage): show developers count stat on the homepage

--- src/controllers/stats.js
+++ src/controllers/stats.js
@@ -1,9 +1,11 @@
 import Word from '../models/Word';
 import Example from '../models/Example';
+import Developer from '../models/Developer';
 import {
   searchForAllWordsWithAudioPronunciations,
   searchForAllWordsWithIsStandardIgbo,
   searchForAllWordsWithNsibidi,
+  searchForAllDevelopers,
 } from './utils/queries';
 
 export const getStats = async (_, res, next) => {
@@ -13,12 +15,14 @@ export const getStats = async (_, res, next) => {
     const totalAudioPronunciations = await Word.find(searchForAllWordsWithAudioPronunciations());
     const totalStandardIgboWords = await Word.find(searchForAllWordsWithIsStandardIgbo());
     const totalNsibidiWords = await Word.find(searchForAllWordsWithNsibidi());
+    const totalDevelopers = await Developer.find(searchForAllDevelopers());
     return res.send({
       totalWords,
       totalExamples,
       totalAudioPronunciations: totalAudioPronunciations.length,
       totalStandardIgboWords: totalStandardIgboWords.length,
       totalNsibidiWords: totalNsibidiWords.length,
+      totalDevelopers: totalDevelopers.length,
     });
   } catch (err) {
     return next(err);
