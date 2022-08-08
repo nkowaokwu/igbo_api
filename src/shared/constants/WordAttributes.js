3da5e6598c60f5968f5cf0a333d13c26d54470ac
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-08-07 20:23:21 -0700

    feat: add the isBorrowedTerm attribute for words

--- src/shared/constants/WordAttributes.js
+++ src/shared/constants/WordAttributes.js
@@ -19,4 +19,8 @@ export default {
     value: 'isConstructedTerm',
     label: 'Is Constructed Term',
   },
+  IS_BORROWED_TERM: {
+    value: 'isBorrowedTerm',
+    label: 'Is Borrowed Term',
+  },
 };
