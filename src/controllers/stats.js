37504dea83080954f93c06bba39b92d8f6d0907d
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2022-11-22 22:06:07 -0800

    fix(stats): use stat schema to improve performance for loading stats

--- src/controllers/stats.js
+++ src/controllers/stats.js
@@ -1,24 +1,19 @@
-import { wordSchema } from '../models/Word';
-import { exampleSchema } from '../models/Example';
-import Developer from '../models/Developer';
-import {
-  searchForAllWordsWithAudioPronunciations,
-  searchForAllWordsWithIsStandardIgbo,
-  searchForAllWordsWithNsibidi,
-  searchForAllDevelopers,
-} from './utils/queries';
+import { statSchema } from '../models/Stat';
+import { developerSchema } from '../models/Developer';
+import { searchForAllDevelopers } from './utils/queries';
 import { createDbConnection, handleCloseConnection } from '../services/database';
+import StatTypes from '../shared/constants/StatTypes';
 
 export const getStats = async (_, res, next) => {
   const connection = createDbConnection();
-  const Word = connection.model('Word', wordSchema);
-  const Example = connection.model('Example', exampleSchema);
+  const Stat = connection.model('Stat', statSchema);
+  const Developer = connection.model('Developer', developerSchema);
   try {
-    const totalWords = await Word.countDocuments();
-    const totalExamples = await Example.countDocuments();
-    const totalAudioPronunciations = await Word.find(searchForAllWordsWithAudioPronunciations());
-    const totalStandardIgboWords = await Word.find(searchForAllWordsWithIsStandardIgbo());
-    const totalNsibidiWords = await Word.find(searchForAllWordsWithNsibidi());
+    const totalWords = Stat.findOne({ type: StatTypes.SUFFICIENT_WORDS });
+    const totalExamples = Stat.findOne({ type: StatTypes.SUFFICIENT_EXAMPLES });
+    const totalAudioPronunciations = Stat.findOne({ type: StatTypes.HEADWORD_AUDIO_PRONUNCIATIONS });
+    const totalStandardIgboWords = Stat.findOne({ type: StatTypes.STANDARD_IGBO });
+    const totalNsibidiWords = Stat.findOne({ type: StatTypes.NSIBIDI_WORDS });
     const totalDevelopers = await Developer.find(searchForAllDevelopers());
     await handleCloseConnection(connection);
     return res.send({
---
+++ src/shared/constants/StatTypes.js
@@ -0,0 +1,12 @@
+export default {
+  SUFFICIENT_WORDS: 'sufficient_words',
+  COMPLETE_WORDS: 'complete_words',
+  SUFFICIENT_EXAMPLES: 'sufficient_examples',
+  COMPLETE_EXAMPLES: 'complete_examples',
+  DIALECTAL_VARIATIONS: 'dialectal_variations',
+  HEADWORD_AUDIO_PRONUNCIATIONS: 'headword_audio_pronunciations',
+  STANDARD_IGBO: 'standard_igbo',
+  NSIBIDI_WORDS: 'nsibidi_words',
+  NSIBIDI_WORD_SUGGESTIONS: 'nsibidi_word_suggestions',
+  USER: 'user',
+};
