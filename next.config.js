7b8de4fa440f3629b4884744fca296f3b041624f
temtechie <templendukwu144@gmail.com>
2023-03-13 15:29:41 +0100

    fix: resolved PR comments

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { i18n } = require('./next-i18next.config');
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
-  
 });
 
 module.exports = (_, { defaultConfig }) => ({
