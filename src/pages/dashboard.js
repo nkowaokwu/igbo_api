4729c65642d9e9ee6320de7aa03a4295822a43aa
davydocsurg <davydocsurg@gmail.com>
2023-04-28 16:48:40 +0100

    update remote branch #598

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,6 @@
 import { Heading } from '@chakra-ui/react';
 import React from 'react';
 
-const Dashboard = () => <Heading as="h1">Dashboard page</Heading>;
+const Dashboard = () => <Heading as="h1">Dashboard</Heading>;
 
 export default Dashboard;
