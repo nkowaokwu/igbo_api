f67673d8fcce5d1e22e056304f8d9f31a91d93ec
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-05-04 08:28:38 -0400

    Revert "Revert "Revert "Revert "Release Updated Docs Site - Revert "Revert "Updated Docs Site""""""

--- functions/next.config.js
+++ functions/next.config.js
@@ -1,7 +1,12 @@
+const nextra = require('nextra');
 const { i18n } = require('./next-i18next.config');
 
-module.exports = (_, { defaultConfig }) => ({
-  ...defaultConfig,
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
+module.exports = withNextra({
   distDir: 'build/dist',
   generateBuildId: async () => 'api-homepage',
   i18n,
