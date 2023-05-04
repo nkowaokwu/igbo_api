ba3c41f3b680161af8357ffdd881e9de0803fe92
Joel <ndohjoelmbj16@gmail.com>
2023-05-04 14:40:39 +0100

    fix(ui): fix element issue using chakra ui Box component

---
+++ src/pages/dashboard.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+import { Heading } from '@chakra-ui/react';
+import React from 'react';
+
+const Dashboard = () => <Heading as="h1">Dashboard</Heading>;
+
+export default Dashboard;
