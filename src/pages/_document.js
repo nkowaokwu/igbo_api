5681d1683fed38cb5b4370d5e30c4d6f0d60a5b9
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-05-04 21:53:14 -0400

    chore(site): add image preview and other meta tag information

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import Document, {
   Html,
@@ -16,9 +17,32 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+          {/* Metadata */}
+          <meta name="title" content="Igbo API - The First African Language API" />
+          <meta name="description" content="Igbo Dictionary API" />
+          <meta property="og:title" content="Igbo API - The First African Language API" />
+          <meta property="og:description" content="Igbo Dictionary API" />
+          <meta property="twitter:title" content="Igbo API - The First African Language API" />
+          <meta property="twitter:description" content="Igbo Dictionary API" />
+          <meta property="twitter:image:alt" content="Igbo API - The First African Language API" />
+
+          {/* Meta image */}
+          <meta property="og:image" content="https://nkowaokwu.s3.us-west-1.amazonaws.com/assets/icons/igbo_api/igbo_api_banner.png" />
+          <meta property="twitter:image" content="https://nkowaokwu.s3.us-west-1.amazonaws.com/assets/icons/igbo_api/igbo_api_banner.png" />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://igboapi.com/" />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@nkowaokwu" />
+          <meta property="twitter:url" content="https://igboapi.com/" />
+
+          {/* Favicon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="https://nkowaokwu.s3.us-west-1.amazonaws.com/assets/icons/igbo_api/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="https://nkowaokwu.s3.us-west-1.amazonaws.com/assets/icons/igbo_api/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="https://nkowaokwu.s3.us-west-1.amazonaws.com/assets/icons/igbo_api/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/assets/favicon/site.webmanifest" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
         </Head>
