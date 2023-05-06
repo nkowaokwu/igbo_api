5da183fbfb926a53146939f861242ce596c82732
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-05-06 18:34:15 -0400

    fix: look up API token with index then collection scan

--- src/middleware/validateApiKey.js
+++ src/middleware/validateApiKey.js
@@ -3,12 +3,62 @@ import {
   isDevelopment,
   isProduction,
 } from '../config';
-import {
-  handleRequest,
-  FALLBACK_API_KEY,
-  checkDeveloperAPILimit,
-  findDeveloper,
-} from '../controllers/developers/utils';
+import { createDbConnection } from '../services/database';
+
+const PROD_LIMIT = 2500;
+const FALLBACK_API_KEY = 'fallback_api_key';
+
+const determineLimit = (apiLimit) => (
+  isTest
+    ? apiLimit || PROD_LIMIT
+    : PROD_LIMIT
+);
+
+const isSameDate = (first, second) => (
+  first.getFullYear() === second.getFullYear()
+    && first.getMonth() === second.getMonth()
+    && first.getDate() === second.getDate()
+);
+
+/* Increments usage count and updates usage date */
+const handleDeveloperUsage = async (developer) => {
+  const updatedDeveloper = developer;
+  const isNewDay = !isSameDate(updatedDeveloper.usage.date, new Date());
+  updatedDeveloper.usage.date = Date.now();
+
+  if (isNewDay) {
+    updatedDeveloper.usage.count = 0;
+  } else {
+    updatedDeveloper.usage.count += 1;
+  }
+
+  return updatedDeveloper.save();
+};
+
+/* Finds a developer with provided information */
+const findDeveloper = async (apiKey) => {
+  console.time('Finding developer account');
+  const connection = createDbConnection();
+  const Developer = connection.model('Developer', developerSchema);
+  let developer = await Developer.findOne({ apiKey });
+  if (developer) {
+    console.timeEnd('Finding developer account');
+    return developer;
+  }
+  // Legacy implementation: hashed API tokens can't be indexed
+  // This logic attempts to find the developer document and update it
+  // with the API token
+  const developers = await Developer.find({});
+  developer = developers.find((dev) => compareSync(apiKey, dev.apiKey));
+  if (developer) {
+    developer.apiKey = apiKey;
+    const updatedDeveloper = await developer.save();
+    console.timeEnd('Finding developer account');
+    return updatedDeveloper;
+  }
+  console.timeEnd('Finding developer account');
+  return developer;
+};
 
 export default async (req, res, next) => {
   try {
