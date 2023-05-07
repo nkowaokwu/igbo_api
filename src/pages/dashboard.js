ef0891cdb6bdc8b49bb12a6afbb79b6bdfe25559
davydocsurg <davydocsurg@gmail.com>
2023-05-07 20:30:48 +0100

    chore: add User info component #619

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,21 @@
 import { Heading } from '@chakra-ui/react';
 import React from 'react';
+import UserInfo from './components/UserInformation';
 
-const Dashboard = () => <Heading as="h1">Dashboard</Heading>;
+const Dashboard = () => (
+  <div
+    className="flex flex-col items-center h-screen"
+    // style={{
+    //   display: 'flex',
+    //   flexDirection: 'column',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
+    //   height: '100vh',
+    // }}
+  >
+    <Heading as="h1">Dashboard</Heading>
+    <UserInfo />
+  </div>
+);
 
 export default Dashboard;
