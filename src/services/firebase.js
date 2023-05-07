02172eed33f36add564502d4a0100ce4b13f8c29
davydocsurg <davydocsurg@gmail.com>
2023-05-07 20:32:05 +0100

    add User info component #619

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,20 +3,13 @@ import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
 import { isProduction } from '../config';
 
 const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyDA4Osryb094cI6ER3-iv7dtKqniHZJo0o',
-  authDomain: 'nkowaokwu-davydocsurg.firebaseapp.com',
-  projectId: 'nkowaokwu-davydocsurg',
-  storageBucket: 'nkowaokwu-davydocsurg.appspot.com',
-  messagingSenderId: '466044324666',
-  appId: '1:466044324666:web:078a574d09a3380fc75e03',
-  measurementId: 'G-9NPCPQ8S76',
-  // apiKey: 'AIzaSyBDXPLmvu7YEagwdgp_W4uoZhCglbXrG6M',
-  // authDomain: 'igbo-api-bb22d.firebaseapp.com',
-  // projectId: 'igbo-api-bb22d',
-  // storageBucket: 'igbo-api-bb22d.appspot.com',
-  // messagingSenderId: '299917108135',
-  // appId: '1:299917108135:web:e5053fceeba3155b624c82',
-  // measurementId: 'G-YGGV667F2H',
+  apiKey: 'AIzaSyBDXPLmvu7YEagwdgp_W4uoZhCglbXrG6M',
+  authDomain: 'igbo-api-bb22d.firebaseapp.com',
+  projectId: 'igbo-api-bb22d',
+  storageBucket: 'igbo-api-bb22d.appspot.com',
+  messagingSenderId: '299917108135',
+  appId: '1:299917108135:web:e5053fceeba3155b624c82',
+  measurementId: 'G-YGGV667F2H',
 };
 
 initializeApp(FIREBASE_CONFIG);
