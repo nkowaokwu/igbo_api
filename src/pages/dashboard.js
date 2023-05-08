f45053b1547a71cb3481543056c58336e1022da1
davydocsurg <davydocsurg@gmail.com>
2023-05-08 10:34:42 +0100

    chore: update User info component #619

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,21 +1,25 @@
-import { Heading } from '@chakra-ui/react';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import UserInfo from './components/UserInformation';
+import Navbar from './components/Navbar';
 
-const Dashboard = () => (
-  <div
-    className="flex flex-col items-center h-screen"
-    // style={{
-    //   display: 'flex',
-    //   flexDirection: 'column',
-    //   alignItems: 'center',
-    //   justifyContent: 'center',
-    //   height: '100vh',
-    // }}
-  >
-    <Heading as="h1">Dashboard</Heading>
-    <UserInfo />
-  </div>
-);
+const Dashboard = () => {
+  const { t } = useTranslation('dashboard');
+
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <Navbar to="/" />
+      <div
+        className="flex flex-col px-8 mb-6 lg:justify-between xl:flex-row pt-10
+      lg:pt-32 max-w-2xl lg:max-w-6xl h-full text-gray-800 text-lg lg:text-xl w-full"
+      >
+        <div className="max-w-3xl space-y-4 mb-10 text-gray-600">
+          <h1 className="text-3xl text-gray-700">{t('Dashboard')}</h1>
+          <UserInfo />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
