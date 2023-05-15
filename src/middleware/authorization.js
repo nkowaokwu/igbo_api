e5b3ccba97083b1bfe4931a0ab735fd7a82a0100
davydocsurg <davydocsurg@gmail.com>
2023-05-15 16:10:51 +0100

    add authorization middleware  #627

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -1,8 +1,29 @@
-import { fetchAPIKey, findDeveloper } from '../shared/constants/DeveloperUtils';
+import { isDevelopment } from '../config';
+import { FALLBACK_API_KEY, fetchAPIKey, findDeveloper } from '../shared/constants/DeveloperUtils';
 
 export default async (req, res, next) => {
-  // const { apiLimit } = req.query;
-  const apiKey = fetchAPIKey();
+  try {
+    // const { apiLimit } = req.query;
+    let apiKey = fetchAPIKey(req);
 
-  const developer = await findDeveloper(apiKey);
+    if (!apiKey && isDevelopment) {
+      apiKey = FALLBACK_API_KEY;
+    }
+
+    const developer = await findDeveloper(apiKey);
+
+    if (!developer) {
+      throw new Error('Developer account not found');
+    }
+
+    //   check if api key belongs to developer
+    if (developer.apiKey !== apiKey) {
+      throw new Error('Invalid API Key. Check your API Key and try again');
+    }
+
+    return next();
+  } catch (err) {
+    res.status(400);
+    return res.send({ error: err.message });
+  }
 };
