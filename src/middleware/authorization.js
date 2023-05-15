976acb5e279647aad35a2662fa43c535c2d6251d
davydocsurg <davydocsurg@gmail.com>
2023-05-15 16:51:47 +0100

    chore: fix up authorization middleware, update findDeveloper helper #627

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -10,16 +10,8 @@ export default async (req, res, next) => {
       apiKey = FALLBACK_API_KEY;
     }
 
-    const developer = await findDeveloper(apiKey);
-
-    if (!developer) {
-      throw new Error('Developer account not found');
-    }
-
     //   check if api key belongs to developer
-    if (developer.apiKey !== apiKey) {
-      throw new Error('Invalid API Key. Check your API Key and try again');
-    }
+    await findDeveloper(apiKey);
 
     return next();
   } catch (err) {
