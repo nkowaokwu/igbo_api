1b967f2b2ce5dbefa14b75c64d9f453a5a121dd1
davydocsurg <davydocsurg@gmail.com>
2023-05-16 10:41:03 +0100

    chore: move developerUtils functions to developers/utils #627

--- src/controllers/developers/index.js
+++ src/controllers/developers/index.js
@@ -1,3 +1,12 @@
 import { getDeveloper, postDeveloper } from './developers';
+import { findDeveloper, FALLBACK_API_KEY, checkDeveloperAPIKey, fetchAPIKey, fetchRequestQuery } from './utils';
 
-export { getDeveloper, postDeveloper };
+export {
+  getDeveloper,
+  postDeveloper,
+  FALLBACK_API_KEY,
+  checkDeveloperAPIKey,
+  fetchAPIKey,
+  fetchRequestQuery,
+  findDeveloper,
+};
