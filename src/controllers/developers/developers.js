a8babc404141ea77ad36589d5720ff2142e3c435
davydocsurg <davydocsurg@gmail.com>
2023-05-17 10:19:58 +0100

    chore: update get developer test #627

--- src/controllers/developers/developers.js
+++ src/controllers/developers/developers.js
@@ -70,7 +70,7 @@ export const getDeveloper = async (req, res, next) => {
     }
 
     await handleCloseConnection(connection);
-    return res.send({
+    return res.status.send({
       message: 'Success',
       developer,
     });
