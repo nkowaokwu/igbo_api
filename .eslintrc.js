c3bb5dcdf91d6629eda79adfddab329ab2e65c3b
Gospel Chinyereugo <gospelokpara@gmail.com>
2023-05-20 16:33:59 +0100

    fix: feedback fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,6 @@ module.exports = {
         'airbnb',
         'airbnb-typescript',
         'plugin:cypress/recommended',
-        'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
       ],
