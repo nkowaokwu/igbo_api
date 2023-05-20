4ec50025f8ff0bca1ccbd0c68b117d43cb7829b8
Gospel Chinyereugo <gospelokpara@gmail.com>
2023-05-20 21:00:40 +0100

    fix: feedback fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,11 +35,6 @@ module.exports = {
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
-      env: {
-        es2021: true,
-        node: true,
-        jest: true,
-      },
       extends: [
         'next',
         'airbnb',
@@ -56,23 +51,6 @@ module.exports = {
       },
       plugins: ['@typescript-eslint', 'eslint-plugin-jsx-a11y', 'cypress'],
       rules: {
-        'function-call-argument-newline': ['off'],
-        'function-paren-newline': ['off'],
-        'import/prefer-default-export': ['off'],
-        'max-len': ['error', { code: 120 }],
-        'newline-per-chained-call': ['off'],
-        'no-console': ['off'],
-        'no-nested-ternary': ['off'],
-        'no-promise-executor-return': ['off'],
-        'no-underscore-dangle': ['off'],
-        semi: ['error', 'always'],
-        'react/jsx-filename-extension': ['off'],
-        'react/function-component-definition': ['off'],
-        'react/jsx-props-no-spreading': ['off'],
-        'react/no-danger': ['off'],
-        '@next/next/no-html-link-for-pages': ['off'],
-        '@next/next/no-img-element': ['off'],
-        'linebreak-style': ['error', os.platform() === 'win32' ? 'windows' : 'unix'],
         '@typescript-eslint/no-unused-vars': [
           'warn',
           {
