19b219bfce1fcc081d30ea8af5270cb2fc556284
Gospel Chinyereugo <gospelokpara@gmail.com>
2023-05-20 07:31:13 +0100

    fix: fix NODE_ENV type declaration error

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { PORT } from './config';
 import app from './app';
+import NodeEnv from './shared/constants/NodeEnv';
 
 const server = app.listen(PORT, () => {
   console.green(`🟢 Server started on port ${PORT}`);
 
   /* Used to test server build */
-  if (process.env.NODE_ENV === 'build') {
+  if (NodeEnv === 'build') {
     console.blue('🧪 Testing server build');
     setTimeout(() => {
       console.green('✅ Build test passed');
