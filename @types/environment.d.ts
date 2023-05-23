85e8deac03fb82b692186e2c04a97d4d63dd6f95
Gospel Chinyereugo <gospelokpara@gmail.com>
2023-05-23 03:22:49 +0100

    fix: feedback fixes, add @ts-expect-error to Nodejs process override

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -1,6 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      // @ts-expect-error  Nodejs process override
       readonly NODE_ENV: 'development' | 'production' | 'test' | 'build';
     }
   }
