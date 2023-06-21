ec4b4178cf89f2b950246ccddbd77439bf263880
Samson Nwokike <nwokikesamson@gmail.com>
2023-06-21 12:10:19 +0100

    feat: convert src/pages/components/Card to ts

---
+++ src/pages/components/Card/Card.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Tooltip } from '@chakra-ui/react';
+import { ReactI18NextChild } from 'react-i18next';
+
+interface CardInterface {
+  title: string;
+  description: string;
+  icon: ReactI18NextChild;
+  tooltipLabel: string;
+}
+
+export default function Card({ title, description, icon, tooltipLabel }: CardInterface) {
+  return (
+    <Tooltip label={tooltipLabel}>
+      <div
+        style={{
+          maxHeight: 250,
+          maxWidth: 400,
+        }}
+        className={`w-full flex flex-col items-center py-4 cursor-default
+      shadow-md rounded-lg px-5 my-10 bg-gradient-to-t from-gray-50 to-white`}
+      >
+        <div className="flex flex-row items-center space-x-2 justif-start">
+          <span className="justify-center w-16 my-4 text-3xl text-center bg-white rounded-full">{icon}</span>
+          <div>
+            <h1 className="text-2xl font-bold text-gray-900">{title}</h1>
+            <p
+              style={{
+                maxHeight: 200,
+                maxWidth: 400,
+              }}
+              className="text-gray-500 text-l"
+            >
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </Tooltip>
+  );
+}
+
+// Card.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   description: PropTypes.string.isRequired,
+//   icon: PropTypes.string.isRequired,
+//   tooltipLabel: PropTypes.string.isRequired,
+// };
--- src/shared/constants/parseFileLocations.js
+++ src/shared/constants/parseFileLocations.js
@@ -1,6 +1,6 @@
 const mainPath = `${__dirname}/../..`;
-export const READ_FILE_FORMAT = 'utf8';
 
+export const READ_FILE_FORMAT = 'utf8';
 export const DICTIONARIES_DIR =
   process.env.NODE_ENV === 'test'
     ? `${mainPath}/../__tests__/__mocks__/dictionaries`
