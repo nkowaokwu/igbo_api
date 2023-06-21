585d1eaa1dfebc1ef90b1c91bf6a7ccbe371488e
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-21 10:17:12 +0530

    feat: converted src/APIs to TS

--- src/APIs/RedisAPI.ts
+++ src/APIs/RedisAPI.ts
@@ -1,8 +1,14 @@
+import { RedisClient } from 'redis'; // Import the appropriate Redis client type
 import assign from 'lodash/assign';
 import { REDIS_CACHE_EXPIRATION } from '../config';
 import minimizeWords from '../controllers/utils/minimizeWords';
 
-export const getCachedWords = async ({ key, redisClient }) => {
+interface GetCachedWordsParams {
+  key: string;
+  redisClient: RedisClient;
+}
+
+export const getCachedWords = async ({ key, redisClient }: GetCachedWordsParams) => {
   console.time('Getting cached words');
   const rawCachedWords = await redisClient.get(key);
   const cachedWords = typeof rawCachedWords === 'string' ? JSON.parse(rawCachedWords) : rawCachedWords;
@@ -11,13 +17,15 @@ export const getCachedWords = async ({ key, redisClient }) => {
   return cachedWords;
 };
 
-export const setCachedWords = async ({
-  key,
-  data,
-  redisClient,
-  version,
-}) => {
-  const updatedData = assign(data);
+interface SetCachedWordsParams {
+  key: string;
+  data: any; // Update the type of `data` accordingly
+  redisClient: RedisClient;
+  version: string; // Update the type of `version` accordingly
+}
+
+export const setCachedWords = async ({ key, data, redisClient, version }: SetCachedWordsParams) => {
+  const updatedData = assign({}, data);
   updatedData.words = minimizeWords(data.words, version);
   if (!redisClient.isFake) {
     await redisClient.set(key, JSON.stringify(updatedData), { EX: REDIS_CACHE_EXPIRATION });
@@ -25,35 +33,59 @@ export const setCachedWords = async ({
   return updatedData;
 };
 
-export const getCachedExamples = async ({ key, redisClient }) => {
+interface GetCachedExamplesParams {
+  key: string;
+  redisClient: RedisClient;
+}
+
+export const getCachedExamples = async ({ key, redisClient }: GetCachedExamplesParams) => {
   const rawCachedExamples = await redisClient.get(key);
   const cachedExamples = typeof rawCachedExamples === 'string' ? JSON.parse(rawCachedExamples) : rawCachedExamples;
   console.log(`Retrieved cached data for examples ${key}:`, !!cachedExamples);
   return cachedExamples;
 };
 
-export const setCachedExamples = async ({ key, data, redisClient }) => {
+interface SetCachedExamplesParams {
+  key: string;
+  data: any; // Update the type of `data` accordingly
+  redisClient: RedisClient;
+}
+
+export const setCachedExamples = async ({ key, data, redisClient }: SetCachedExamplesParams) => {
   if (!redisClient.isFake) {
     await redisClient.set(key, JSON.stringify(data), { EX: REDIS_CACHE_EXPIRATION });
   }
   return data;
 };
 
-export const getAllCachedVerbsAndSuffixes = async ({ key, redisClient }) => {
+interface GetAllCachedVerbsAndSuffixesParams {
+  key: string;
+  redisClient: RedisClient;
+}
+
+export const getAllCachedVerbsAndSuffixes = async ({ key, redisClient }: GetAllCachedVerbsAndSuffixesParams) => {
   const redisAllVerbsAndSuffixesKey = `verbs-and-suffixes-${key}`;
   const rawCachedAllVerbsAndSuffixes = await redisClient.get(redisAllVerbsAndSuffixesKey);
-  const cachedAllVerbsAndSuffixes = typeof rawCachedAllVerbsAndSuffixes === 'string'
-    ? JSON.parse(rawCachedAllVerbsAndSuffixes)
-    : rawCachedAllVerbsAndSuffixes;
+  const cachedAllVerbsAndSuffixes =
+    typeof rawCachedAllVerbsAndSuffixes === 'string'
+      ? JSON.parse(rawCachedAllVerbsAndSuffixes)
+      : rawCachedAllVerbsAndSuffixes;
   return cachedAllVerbsAndSuffixes;
 };
+
+interface SetAllCachedVerbsAndSuffixesParams {
+  key: string;
+  data: any; // Update the type of `data` accordingly
+  redisClient: RedisClient;
+  version: string; // Update the type of `version` accordingly
+}
 
 export const setAllCachedVerbsAndSuffixes = async ({
   key,
   data,
   redisClient,
   version,
-}) => {
+}: SetAllCachedVerbsAndSuffixesParams) => {
   const redisAllVerbsAndSuffixesKey = `verbs-and-suffixes-${key}`;
   const updatedData = minimizeWords(data, version);
   if (!redisClient.isFake) {
