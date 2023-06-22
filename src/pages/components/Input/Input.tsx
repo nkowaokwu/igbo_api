ded31a8363ca488738915c4e6611fee0150fab5a
Samson Nwokike <nwokikesamson@gmail.com>
2023-06-22 11:05:51 +0100

    feat: convert src/pages/components/Input to ts

---
+++ src/pages/components/Input/Input.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef, InputHTMLAttributes } from 'react';
+
+interface InputPropsInterface extends InputHTMLAttributes<HTMLInputElement> {
+  header: string;
+  type: string;
+}
+
+type Ref = HTMLInputElement;
+
+const Input = forwardRef<Ref, InputPropsInterface>(({ header, type = '', ...rest }, ref) => (
+  <div className="flex flex-col w-full my-3 items-left">
+    <h3 className="mb-3 font-normal text-gray-600">{header}</h3>
+    <input
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...rest}
+      ref={ref}
+      className="w-full h-10 px-2 border border-gray-300 border-solid rounded-md"
+      type={type}
+    />
+  </div>
+));
+
+export default Input;
