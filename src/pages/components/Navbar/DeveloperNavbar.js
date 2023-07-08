a33cb86ffa10a68aa74fbb4ac218d09b1c2a3552
Joel Ndoh <ndohjoelmbj16@gmail.com>
2023-07-08 23:15:43 +0000

    feat: created the developer navba again

--- src/pages/components/Navbar/DeveloperNavbar.js
+++ src/pages/components/Navbar/DeveloperNavbar.js
@@ -1,81 +1,81 @@
-import React, { useState } from 'react';
-import { Dialog } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  Heading,
+  Text,
+  useDisclosure,
+  DrawerCloseButton,
+} from '@chakra-ui/react';
+import React from 'react';
 
-const navigation = [
-  { name: 'Features', href: '#' },
-  { name: 'Try it Out', href: '#' },
-  { name: 'About', href: '#' },
-  { name: 'Register API Key', href: '#' },
-  { name: 'Docs', href: '#' },
-];
-
-export default function NewHeader() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
+function DeveloperNavbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5">
-              <h1 className="text-2xl font-extrabold">Igbo API</h1>
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-                {item.name}
+    <Box className="flex relative lg:fixed items-center justify-between w-full py-5 lg:px-10">
+      <Box>
+        {' '}
+        <a href="/">
+          <Heading className="ransition-element text-3xl font-extrabold hover:text-gray-700 text-gray-900 ml-5 lg:ml-0">
+            Igbo API
+          </Heading>
+        </a>
+        {' '}
+      </Box>
+      <Box className=" md:hidden">
+        <Button colorScheme="white" className="mr-5 flex-col bg-transparent lg:mr-0" onClick={onOpen}>
+          <span className="h-px bg-gray-700 w-10" />
+          <span className=" mt-4 h-px bg-gray-700 w-10" />
+        </Button>
+        {/* Mobile View */}
+        <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay />
+          <DrawerCloseButton />
+          <DrawerContent className="py-5 lg:px-10">
+            <DrawerCloseButton />
+            <DrawerBody>
+              <a href="/">
+                <Text className="text-xl font-bold py-3">Feature</Text>
               </a>
-            ))}
-          </div>
-        </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto
-           bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-          >
-            <div className="flex items-center justify-between">
-              <a href="/" className="-m-1.5 p-1.5">
-                <h1 className="text-2xl font-extrabold">Igbo API</h1>
+              <a href="/">
+                <Text className="text-xl font-bold py-3">Try it Out</Text>
               </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base
-                      font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
-      </header>
-    </div>
+              <a href="/">
+                <Text className="text-xl font-bold py-3">About</Text>
+              </a>
+              <a href="/">
+                <Text className="text-xl font-bold py-3">Register API Key</Text>
+              </a>
+              <a href="/">
+                <Text className="text-xl font-bold py-3">Docs</Text>
+              </a>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+
+      <Box className="hidden md:flex">
+        <a href="/">
+          <Text className="text-xl font-bold p-3">Feature</Text>
+        </a>
+        <a href="/">
+          <Text className="text-xl font-bold p-3">Try it Out</Text>
+        </a>
+        <a href="/">
+          <Text className="text-xl font-bold p-3">About</Text>
+        </a>
+        <a href="/">
+          <Text className="text-xl font-bold p-3">Register API Key</Text>
+        </a>
+        <a href="/">
+          <Text className="text-xl font-bold p-3">Docs</Text>
+        </a>
+      </Box>
+    </Box>
   );
 }
+
+export default DeveloperNavbar;
