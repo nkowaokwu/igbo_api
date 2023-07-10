68df12b2ec637e3a596e1be6ae0cc2bc33f76107
davydocsurg <davydocsurg@gmail.com>
2023-07-10 13:16:14 +0100

    chore: update auth test #704

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -2,7 +2,10 @@ import { loginDeveloper } from './shared/commands';
 
 describe('login', () => {
   it('should hit the login endpoint and return a message', async () => {
-    const data = {};
+    const data = {
+      email: 'test@email.com',
+      password: 'password',
+    };
     const login = await loginDeveloper(data);
     expect(login.status).toEqual(200);
     expect(login.body.message).toEqual('Logging in...');
