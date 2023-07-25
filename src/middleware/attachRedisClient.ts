29b9dc7b6d8095d28cca21bb3d05f1367c93d3eb
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-07-25 10:25:16 -0400

    Update attachRedisClient.ts

--- src/middleware/attachRedisClient.ts
+++ src/middleware/attachRedisClient.ts
@@ -12,31 +12,34 @@ const afterResponse = (redisClient) => {
 };
 
 // Keep the same Redis Client connection open
-let redisClient;
+const redisClient =
+  REDIS_HOST && REDIS_PORT && REDIS_USERNAME && REDIS_PASSWORD
+    ? createClient({
+        socket: {
+          host: REDIS_HOST,
+          port: REDIS_PORT,
+        },
+        username: REDIS_USERNAME,
+        password: REDIS_PASSWORD,
+      })
+    : REDIS_URL && process.env.FIREBASE_FUNCTIONS
+    ? createClient({
+        url: 'redis://localhost:6379',
+      })
+    : {
+        set: () => null,
+        get: () => null,
+        on: () => console.log('\nFake Redis client'),
+        connect: () => console.log('Connected to fake Redis client'),
+        isFake: true,
+        isReady: true,
+      };
+
 export default async (req, res, next) => {
-  redisClient =
-    REDIS_HOST && REDIS_PORT && REDIS_USERNAME && REDIS_PASSWORD && !redisClient
-      ? createClient({
-          socket: {
-            host: REDIS_HOST,
-            port: REDIS_PORT,
-          },
-          username: REDIS_USERNAME,
-          password: REDIS_PASSWORD,
-        })
-      : REDIS_URL && process.env.FIREBASE_FUNCTIONS
-      ? createClient({
-          url: 'redis://localhost:6379',
-        })
-      : {
-          set: () => null,
-          get: () => null,
-          on: () => console.log('\nFake Redis client'),
-          connect: () => console.log('Connected to fake Redis client'),
-          isFake: true,
-        };
+  if (!redisClient.isReady) {
+    redisClient.connect();
+  }
   redisClient.on('error', (err) => console.log('Redis Client Error', err));
-  redisClient.connect();
 
   res.on('finish', afterResponse);
   res.on('close', afterResponse);
