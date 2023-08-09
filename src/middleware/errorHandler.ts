228eabff9806bcb1f09b505a90e28a349147efb6
Chidi <chidex5@gmail.com>
2023-08-09 15:49:24 +0100

    chore: remove any type and re-add removed arguments

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,6 @@
-import { Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export default (err: any, res: Response) => {
+export default (err, req: Request, res: Response, next: NextFunction) => {
   res.status(400);
   /* Depending on the nested error message the status code will change */
   if (err.message.match(/No .{1,} exist(s)?/) || err.message.match(/doesn't exist(s)?/)) {
