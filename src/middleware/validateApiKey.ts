fbb5c93df1e2c24f003748c9db00dfeb253eb2f5
Chidi <chidex5@gmail.com>
2023-08-09 15:50:08 +0100

    chore: remove any type and remove variable undefine check

--- src/middleware/validateApiKey.ts
+++ src/middleware/validateApiKey.ts
@@ -7,7 +7,7 @@ import { createDbConnection } from '../services/database';
 const PROD_LIMIT = 2500;
 const FALLBACK_API_KEY = 'fallback_api_key';
 
-const determineLimit = (apiLimit: any) => (isTest ? apiLimit || PROD_LIMIT : PROD_LIMIT);
+const determineLimit = (apiLimit) => (isTest ? apiLimit || PROD_LIMIT : PROD_LIMIT);
 
 const isSameDate = (first: Date, second: Date) =>
   first.getFullYear() === second.getFullYear() &&
@@ -15,7 +15,7 @@ const isSameDate = (first: Date, second: Date) =>
   first.getDate() === second.getDate();
 
 /* Increments usage count and updates usage date */
-const handleDeveloperUsage = async (developer: any) => {
+const handleDeveloperUsage = async (developer) => {
   const updatedDeveloper = developer;
   const isNewDay = !isSameDate(updatedDeveloper.usage.date, new Date());
   updatedDeveloper.usage.date = Date.now();
@@ -30,7 +30,7 @@ const handleDeveloperUsage = async (developer: any) => {
 };
 
 /* Finds a developer with provided information */
-const findDeveloper = async (apiKey: string | string[]) => {
+const findDeveloper = async (apiKey) => {
   console.time('Finding developer account');
   const connection = createDbConnection();
   const Developer = connection.model('Developer', developerSchema);
@@ -43,9 +43,9 @@ const findDeveloper = async (apiKey: string | string[]) => {
   // This logic attempts to find the developer document and update it
   // with the API token
   const developers = await Developer.find({});
-  developer = developers.find((dev) => compareSync(apiKey as string, dev.apiKey))!;
+  developer = developers.find((dev) => compareSync(apiKey, dev.apiKey));
   if (developer) {
-    developer.apiKey = apiKey as string;
+    developer.apiKey = apiKey;
     const updatedDeveloper = await developer.save();
     console.timeEnd('Finding developer account');
     return updatedDeveloper;
@@ -84,7 +84,7 @@ export default async (req: UsingMainKeyRequest, res: Response, next: NextFunctio
 
     const developer = await findDeveloper(apiKey);
 
-    if (developer && developer.usage) {
+    if (developer) {
       if (developer.usage.count >= determineLimit(apiLimit)) {
         res.status(403);
         return res.send({ error: 'You have exceeded your limit of requests for the day' });
@@ -95,7 +95,7 @@ export default async (req: UsingMainKeyRequest, res: Response, next: NextFunctio
 
     res.status(401);
     return res.send({ error: 'Your API key is invalid' });
-  } catch (err: any) {
+  } catch (err) {
     res.status(400);
     return res.send({ error: err.message });
   }
