3aa5538796c2220d7050d2c78ebb9aed50a03927
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-08-25 14:52:45 -0400

    chore: optimize dev and prod build

--- scripts/build_src.sh
+++ scripts/build_src.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-babel -d dist/ ./src -s --extensions '.js,.jsx,.ts,.tsx' --ignore '**/*.test.js,**/*.test.ts'
+./node_modules/.bin/babel -d dist/ ./src -s --extensions '.js,.jsx,.ts,.tsx' --ignore '**/*.test.js,**/*.test.ts'
+./node_modules/.bin/babel -d dist/server/pages ./src/pages --extensions '.js,.jsx,.ts,.tsx' --ignore '**/*.test.js,**/*.test.ts'
