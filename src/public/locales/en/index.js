28f81740803750d38cd2a0942d12bf35831d5814
davydocsurg <davydocsurg@gmail.com>
2023-10-11 17:37:00 +0100

    chore: add translations #753

--- src/public/locales/en/index.js
+++ src/public/locales/en/index.js
@@ -1,9 +1,11 @@
 import about from './about.json';
 import common from './common.json';
 import signup from './signup.json';
+import dashboard from './dashboard.json';
 
 export default {
   about,
   common,
   signup,
+  dashboard,
 };
