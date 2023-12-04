6f54cf7a62ed03d4a071bf5b4b75bf577cae8c52
davydocsurg <davydocsurg@gmail.com>
2023-12-05 00:33:20 +0100

    chore: clean up #757

--- src/pages/components/Sidebar/SidebarItem.tsx
+++ src/pages/components/Sidebar/SidebarItem.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, FlexProps } from '@chakra-ui/react';
 
 interface IconType {
   size?: string | number | boolean;
 }
 
-interface SidebarItemProps {
+interface SidebarItemProps extends FlexProps {
   icon: React.ReactElement<IconType>;
   href: string;
   children: React.ReactNode;
   activeBgColor?: string;
   activeTextColor?: string;
+  _active?: {
+    bg: string;
+  };
+  _hover?: {
+    bg?: string;
+    color?: string;
+  };
+  mb?: number;
 }
 
-const SidebarItem = ({ icon, href, children, activeBgColor, activeTextColor }: SidebarItemProps) => (
+const SidebarItem = ({
+  icon,
+  href,
+  children,
+  activeBgColor,
+  activeTextColor,
+  _active,
+  _hover,
+  mb,
+}: SidebarItemProps) => (
   <Box as="a" href={href} style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
     <Flex
       align="center"
@@ -22,12 +39,16 @@ const SidebarItem = ({ icon, href, children, activeBgColor, activeTextColor }: S
       borderRadius="lg"
       role="group"
       cursor="pointer"
-      _hover={{
-        bg: 'cyan.400',
-        color: 'white',
-      }}
+      _hover={
+        _hover && {
+          bg: 'cyan.400',
+          color: 'white',
+        }
+      }
+      _active={_active}
       bg={activeBgColor}
       color={activeTextColor}
+      mb={mb}
     >
       {icon}
       {children}
