8404e85144104493fb28295b6371d99d4b0f2b56
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2024-02-29 09:27:29 -0500

    chore: synced package.json files

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
 const functions = require('firebase-functions');
-const { api } = require('../dist/src/app');
+const { api } = require('./dist/src/app');
 
 exports.api = functions.https.onRequest(api);
