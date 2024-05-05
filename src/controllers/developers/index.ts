8d085445a66249d8ae03abbcd8f8d37aa5a38d6a
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2024-05-05 17:11:42 -0400

    chore: fallback to creating a developer if it doesnt exist

--- src/controllers/developers/index.ts
+++ src/controllers/developers/index.ts
@@ -114,7 +114,12 @@ export const putDeveloper: MiddleWare = async (req, res, next) => {
 
   try {
     return res.send(await putDeveloperHelper({ query: { _id: id }, data: req.body }));
-  } catch (err) {
+  } catch (err: any) {
+    // TODO: write test to make sure that when there's no developer, the postDeveloper method
+    // gets called
+    if (err.message === 'No developer to update') {
+      return postDeveloper(req, res, next);
+    }
     return next(err);
   }
 };
