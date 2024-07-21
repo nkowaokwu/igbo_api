ed5d384fb56ed39ddb0bf9e6303d74ec3bffe41b
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2024-07-21 15:35:52 -0400

    chore(docs): move docs to separate repo and reduce README size

---
+++ script.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const dataset = require('./igbo-api-dataset.json');
+
+const dialects = dataset.flatMap((word) => {
+  if (word.variations) {
+    console.log(word.variations);
+  }
+  return { dialects: word.dialects, variations: word.variations || [] };
+});
+
+// console.log(dialects);
+
+fs.writeFileSync('igbo-api-dialects.json', JSON.stringify(dialects, null, 2));
