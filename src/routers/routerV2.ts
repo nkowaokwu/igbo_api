250107eabf01deda789769826bb955bb49ba0e92
Ebube Chuba <ebube97@me.com>
2024-10-09 14:20:36 -0400

    feat: add translation endpoint and ratelimiter to new route

--- src/routers/routerV2.ts
+++ src/routers/routerV2.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import rateLimit from 'express-rate-limit';
 import { getWords, getWord } from '../controllers/words';
 import { getExample, getExamples } from '../controllers/examples';
 import { getNsibidiCharacter, getNsibidiCharacters } from '../controllers/nsibidi';
@@ -7,6 +8,16 @@ import validId from '../middleware/validId';
 import validateApiKey from '../middleware/validateApiKey';
 import analytics from '../middleware/analytics';
 import attachRedisClient from '../middleware/attachRedisClient';
+import { getTranslation } from 'src/controllers/translation';
+import { MiddleWare } from 'src/types';
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
+const REQUESTS_PER_MS_TRANSLATION = 5;
+
+const translationRateLimiter: MiddleWare = rateLimit({
+  windowMs: ONE_DAY,
+  max: REQUESTS_PER_MS_TRANSLATION,
+});
 
 const routerV2 = Router();
 
@@ -26,6 +37,13 @@ routerV2.get(
 
 // Speech-to-Text
 routerV2.post('/speech-to-text', analytics, validateApiKey, getTranscription);
+routerV2.post(
+  '/igbo-to-english',
+  translationRateLimiter,
+  analytics,
+  validateApiKey,
+  getTranslation
+);
 
 // Redirects to V1
 routerV2.post('/developers', (_, res) => res.redirect('/api/v1/developers'));
