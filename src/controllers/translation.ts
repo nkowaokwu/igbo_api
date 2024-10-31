ec555b6af39e4356297780b10966e18500affdf8
Ebube Chuba <ebube97@me.com>
2024-10-31 09:48:02 -0700

    refactor: update schema for translation

--- src/controllers/translation.ts
+++ src/controllers/translation.ts
@@ -1,10 +1,19 @@
 import axios from 'axios';
 import { MiddleWare } from '../types';
 import { IGBO_TO_ENGLISH_API, MAIN_KEY } from '../config';
+import { z } from 'zod';
+import { fromError } from 'zod-validation-error';
+import LanguageEnum from '../shared/constants/LanguageEnum';
 
-interface TranslationMetadata {
+interface IgboEnglishTranslationMetadata {
   igbo: string;
 }
+
+const TranslationRequestBody = z.object({
+  text: z.string(),
+  sourceLanguageCode: z.nativeEnum(LanguageEnum),
+  destinationLanguageCode: z.nativeEnum(LanguageEnum),
+});
 
 interface Translation {
   translation: string;
@@ -12,7 +21,7 @@ interface Translation {
 
 // Due to limit on inputs used to train the model, the maximum
 // Igbo translation input is 120 characters
-const TRANSLATION_INPUT_MAX_LENGTH = 120;
+const IGBO_ENGLISH_TRANSLATION_INPUT_MAX_LENGTH = 120;
 
 /**
  * Talks to Igbo-to-English translation model to translate the provided text.
@@ -23,18 +32,35 @@ const TRANSLATION_INPUT_MAX_LENGTH = 120;
  */
 export const getTranslation: MiddleWare = async (req, res, next) => {
   try {
-    const igboText: string = req.body['igbo'];
+    const requestBodyValidation = TranslationRequestBody.safeParse(req.body);
+    if (!requestBodyValidation.success) {
+      throw fromError(requestBodyValidation.error);
+    }
+    const requestBody = requestBodyValidation.data;
+
+    if (requestBody.sourceLanguageCode === requestBody.destinationLanguageCode) {
+      throw new Error('Source and destination languages must be different');
+    }
+    if (
+      requestBody.sourceLanguageCode !== LanguageEnum.IGBO ||
+      requestBody.destinationLanguageCode !== LanguageEnum.ENGLISH
+    ) {
+      throw new Error(
+        `${requestBody.sourceLanguageCode} to ${requestBody.destinationLanguageCode} translation is not yet supported`
+      );
+    }
+    const igboText = requestBody.text;
     if (!igboText) {
       throw new Error('Cannot translate empty string');
     }
 
-    if (igboText.length > TRANSLATION_INPUT_MAX_LENGTH) {
+    if (igboText.length > IGBO_ENGLISH_TRANSLATION_INPUT_MAX_LENGTH) {
       throw new Error('Cannot translate text greater than 120 characters');
     }
 
-    const payload: TranslationMetadata = { igbo: igboText };
+    const payload: IgboEnglishTranslationMetadata = { igbo: igboText };
 
-    // Talks to prediction endpoint
+    // Talks to translation endpoint
     const { data: response } = await axios.request<Translation>({
       method: 'POST',
       url: IGBO_TO_ENGLISH_API,
