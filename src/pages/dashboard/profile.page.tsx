99e163554652e7d55afd8f0e97d1c0a5feed2fec
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2024-11-02 12:53:48 -0700

    fix: hide Stripe reference on profile

--- src/pages/dashboard/profile.page.tsx
+++ src/pages/dashboard/profile.page.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Avatar, Box, Heading, Text, Badge } from '@chakra-ui/react';
+import { Avatar, Box, Heading, Text, VStack } from '@chakra-ui/react';
 import DashboardLayout from './layout';
 
 const Profile = () => (
   <DashboardLayout>
     {({ developer }) => (
-      <Box className="flex flex-col justify-center items-center">
+      <VStack width="full" justifyContent="center" gap={2}>
         <Avatar size="lg" />
         <Heading as="h1">Profile</Heading>
         <Box className="space-y-2">
@@ -15,13 +14,13 @@ const Profile = () => (
               {developer.email}
             </Text>
           </Box>
-          <Box className="w-full flex justify-center">
+          {/* <Box className="w-full flex justify-center">
             <Badge colorScheme={developer.stripeId ? 'blue' : 'yellow'}>
               {developer.stripeId ? 'Stripe Connected' : 'Stripe Disconnected'}
             </Badge>
-          </Box>
+          </Box> */}
         </Box>
-      </Box>
+      </VStack>
     )}
   </DashboardLayout>
 );
