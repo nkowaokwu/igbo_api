70eb8d2957f9bf7e1bf349d3ca33fe1772c3415d
Ebube Chuba <ebube97@me.com>
2024-11-04 23:27:35 -0800

    fix: send error response and proper json in ai post requests

--- src/controllers/translation.ts
+++ src/controllers/translation.ts
@@ -73,7 +73,7 @@ export const getTranslation: MiddleWare = async (req, res, next) => {
       data: payload,
     });
 
-    return res.send({ translation: response.translation });
+    return res.status(200).json({ translation: response.translation });
   } catch (err) {
     return next(err);
   }
