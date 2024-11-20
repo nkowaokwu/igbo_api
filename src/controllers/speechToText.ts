c25e30560993fbe3c163b0a0b7d5c2e87d7015ff
Ebube Chuba <ebube97@me.com>
2024-11-20 00:29:35 -0500

    use direct STT endpoint

--- src/controllers/speechToText.ts
+++ src/controllers/speechToText.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { MAIN_KEY, SPEECH_TO_TEXT_API } from '../config';
+import { IGBO_STT_API, MAIN_KEY, SPEECH_TO_TEXT_API } from '../config';
 import Endpoint from '../shared/constants/Endpoint';
 import { MiddleWare } from '../types';
 import { fetchBase64Data } from './utils/fetchBase64Data';
@@ -59,7 +59,7 @@ export const getTranscription: MiddleWare = async (req, res, next) => {
     // Talks to prediction endpoint
     const { data: response } = await axios.request<Prediction>({
       method: 'POST',
-      url: `${SPEECH_TO_TEXT_API}/predict`,
+      url: IGBO_STT_API,
       headers: {
         'Content-Type': 'application/json',
         'X-API-Key': MAIN_KEY,
